bd8937d089b471413f8bafd328a40ca1d9439466
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-12-15 08:08:50 -0500

    added comments to the candlelighting solution

--- CandleLighting.py
+++ CandleLighting.py
@@ -3,9 +3,14 @@ def candleLighting(days:int) -> list[int]:
         return [10, 10, 10, 10, 10, 10, 10, 10, 10]
     else:
         candles = candleLighting(days - 1)
+
+        ## if the day is greater than 10, iterate up to index 9
         upto = min(days - 1, 9)
 
+        ##iterate up to the correct index
         for i in range(upto):
+
+            ##If candles[i] - 1 is less than 0, make it 0
             candles[i] = max(candles[i] - 1, 0)
 
         return candles
