d57db1f100c200db5d0b34a8a4653f791d9a9d34
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-12-15 13:51:15 -0500

    wrote a solution to portals

--- ThinkingWithPortals.py
+++ ThinkingWithPortals.py
@@ -13,14 +13,14 @@ our_graph = {
     12:[11, 13],
     13:[14],
     14:[15, 24],
-    15:[16, 18, 22],
+    15:[16, 18, 22, 14],
     16:[15, 17],
     17:[18],
     18:[15, 19],
     19:[20],
     20:[21],
     21:[22],
-    22:[23],
+    22:[23, 15],
     23:[16],
     24:[25],
     25:[26],
@@ -28,7 +28,29 @@ our_graph = {
     
 }
 
-print(our_graph[1])
+
+def shortestDistance(start:int, end:int) ->int:
+    queue= []
+    distances = {}
+    queue.append(start)
+    distances[start] = 0
+
+
+    if start == end:
+        return 0
+    
+    while True:
+        current = queue.pop(0)
+        for node in our_graph[current]:
+            queue.append(node)
+            distances[node] = distances[current] + 1
+            if node == end:
+                return distances[node]
+    return None
+
+print(shortestDistance(20,25))
+
+    
 
 
 
